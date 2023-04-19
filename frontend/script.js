0ccc0f76399ea81ee2dc5a6363beb6d0857a0846
reichert-p <73558016+reichert-p@users.noreply.github.com>
2023-04-19 11:02:14 +0200

    cool keyboard layout

--- frontend/script.js
+++ frontend/script.js
@@ -1,93 +1,169 @@
-let gameScene;
+class Game extends Phaser.Scene
+{
+
+  cursors;
+  player;
+  emitter;
+
+  keys = [];  
+
+preload ()
+{
+  this.setupKeys()
+
+  this.load.setBaseURL('assets');
+
+  this.load.image('dude', 'character.png');
+  //this.load.image('keyboard', 'keyboard.png');
+  this.load.image('particle', 'fire.png');
+
+  this.keys.forEach((key) => {
+    var name = key+'Key';
+    var location = 'Keys/' + key + '-Key.png'
+    this.load.spritesheet(name , location, { frameWidth: 32, frameHeight: 32 });
+  });
+
+  this.load.spritesheet('ShiftKey', 'Keys/Shift-Key.png', { frameWidth: 48, frameHeight: 32 })
+  this.load.spritesheet('SpaceKey', 'Keys/Space-Key.png', { frameWidth: 64, frameHeight: 32 })
+}
+
+create ()
+{
+  //this.add.image(400, 300, 'keyboard')
+
+  var particles = this.add.particles('particle');
+
+  this.emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 0.2, end: 0 },
+      blendMode: 'ADD',
+  });
+
+  this.player = this.physics.add.sprite(200, 300, 'dude').setOrigin(0);
+  this.player.setSize(35, 35);
+  this.player.setDisplaySize(35, 35);
+
+  this.player.setBounce(0.8);
+  this.player.setCollideWorldBounds(true);
+ 
+  this.cursors = this.input.keyboard.createCursorKeys();
+  this.emitter.startFollow(this.player);
+
+  this.createKeyboardLayout()
+}
+
+createKeyboardLayout(){
+  for (let i = 0; i < 9; i++) {
+    this.createSimpleKey(this.keys[i],50 + 79 * i,50)
+  }
+  this.createSimpleKey(this.keys[35], 50 + 71 * 10, 50)
+
+  this.createSimpleKey(this.keys[25], 50,180)
+  this.createSimpleKey(this.keys[31], 50 + 71 * 1, 180)
+  this.createSimpleKey(this.keys[13], 50 + 71 * 2, 180)
+  this.createSimpleKey(this.keys[26], 50 + 71 * 3, 180)
+  this.createSimpleKey(this.keys[28], 50 + 71 * 4, 180)
+  this.createSimpleKey(this.keys[34], 50 + 71 * 5, 180)
+  this.createSimpleKey(this.keys[29], 50 + 71 * 6, 180)
+  this.createSimpleKey(this.keys[17], 50 + 71 * 7, 180)
+  this.createSimpleKey(this.keys[23], 50 + 71 * 8, 180)
+  this.createSimpleKey(this.keys[24], 50 + 71 * 9, 180)
+  this.createSimpleKey(this.keys[35], 50 + 71 * 10, 180) // ü
+  
+
+  this.createSimpleKey(this.keys[9], 50,310)
+  this.createSimpleKey(this.keys[27], 50 + 71 * 1, 310)
+  this.createSimpleKey(this.keys[12], 50 + 71 * 2, 310)
+  this.createSimpleKey(this.keys[14], 50 + 71 * 3, 310)
+  this.createSimpleKey(this.keys[15], 50 + 71 * 4, 310)
+  this.createSimpleKey(this.keys[16], 50 + 71 * 5, 310)
+  this.createSimpleKey(this.keys[18], 50 + 71 * 6, 310)
+  this.createSimpleKey(this.keys[19], 50 + 71 * 7, 310)
+  this.createSimpleKey(this.keys[20], 50 + 71 * 8, 310)
+  this.createSimpleKey(this.keys[35], 50 + 71 * 9, 310)  // ö
+  this.createSimpleKey(this.keys[35], 50 + 71 * 10, 310)  //ä
+
+
+  this.createSimpleKey(this.keys[33], 170,440)
+  this.createSimpleKey(this.keys[32], 170 + 71 * 1, 440)
+  this.createSimpleKey(this.keys[11], 170 + 71 * 2, 440)
+  this.createSimpleKey(this.keys[30], 170 + 71 * 3, 440)
+  this.createSimpleKey(this.keys[10], 170 + 71 * 4, 440)
+  this.createSimpleKey(this.keys[22], 170 + 71 * 5, 440)
+  this.createSimpleKey(this.keys[21], 170 + 71 * 6, 440)
+
+
+  this.createKey('Space', 250 , 510, 120, 50 )  
+  this.createKey('Shift', 50 , 510, 80, 50 )  
+}
+
+createKey(key, xpos, ypos, xsize, ysize){
+  var keysprite = this.physics.add.sprite(xpos, ypos, key+'Key')
+  keysprite.setSize(xsize, ysize);
+  keysprite.setDisplaySize(xsize, ysize);
+  keysprite.body.allowGravity = false
+
+  this.anims.create({
+    key: key+'Key',
+    frames: this.anims.generateFrameNumbers(key+'Key', { start: 1, end: 0 }),
+    frameRate: 20,
+    repeat: 1
+  });
+
+  this.physics.add.overlap(this.player, keysprite, this.hitKey, null, this);
+}
+
+createSimpleKey(key, xpos, ypos) {
+  this.createKey(key, xpos, ypos, 50, 50)
+}
+
+hitKey (player, key)
+{
+  key.anims.play(key.texture);
+  console.log(key.texture.key)
+}
+
+update() {
+  if (this.cursors.left.isDown)
+  {
+      this.player.setVelocityX(-160);
+  }
+  else if (this.cursors.right.isDown)
+  {
+      this.player.setVelocityX(160)
+  }
+  else
+  {
+      this.player.setVelocityX(0);
+  }
+  if (this.cursors.up.isDown)
+  {
+      this.player.setVelocityY(-330);
+  }
+  }
+
+  setupKeys(){
+    this.keys.push('1','2','3', '4','5','6','7','8','9')
+    this.keys.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+    this.keys.push('Esc')
+  }
+}
+
+
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  parent: "gameCanvas",
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
+  backgroundColor: '#d500d5',
   physics: {
-    default: "arcade",
-    arcade: {
-      //   gravity: { y: 300 },
-      debug: false,
-    },
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 200 }
+      }
   },
-  input: {
-    keyboard: true, // enable keyboard input
-    target: window,
-  },
+  scene: Game
 };
 
 const game = new Phaser.Game(config);
 
-function preload() {
-  gameScene = this;
-  this.load.image("character", "assets/character.png");
-  this.load.image("keyboard", "assets/keyboard.png");
-}
-
-let character;
-let keyboard;
-
-function create() {
-  keyboard = this.add.image(400, 300, "keyboard");
-  character = this.physics.add.sprite(400, 300, "character");
-
-  gameScene.input.keyboard.on("keydown", function (event) {
-    if (event.keyCode === Phaser.Input.Keyboard.KeyCodes.ENTER) {
-      const letter = getLetter(character.x, character.y);
-      document.getElementById("inputBox").value += letter;
-    }
-  });
-}
-
-function getLetter(x, y) {
-  const row = Math.floor((y - 150) / 50);
-  const column = Math.floor((x - 170) / 50);
-  const letters = [
-    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-    ["Z", "X", "C", "V", "B", "N", "M"],
-  ];
-  return letters[row][column];
-}
-
-function update() {
-  // Move the character based on the user's input
-  if (gameScene.input.keyboard.checkDown(Phaser.Input.Keyboard.KeyCodes.LEFT)) {
-    character.setVelocityX(-100);
-  } else if (
-    gameScene.input.keyboard.checkDown(Phaser.Input.Keyboard.KeyCodes.RIGHT)
-  ) {
-    character.setVelocityX(100);
-  } else {
-    character.setVelocityX(0);
-  }
-
-  if (gameScene.input.keyboard.checkDown(Phaser.Input.Keyboard.KeyCodes.UP)) {
-    character.setVelocityY(-100);
-  } else if (
-    gameScene.input.keyboard.checkDown(Phaser.Input.Keyboard.KeyCodes.DOWN)
-  ) {
-    character.setVelocityY(100);
-  } else {
-    character.setVelocityY(0);
-  }
-
-  // Update the character's animation based on its velocity
-  if (character.body.velocity.x < 0) {
-    character.setFlipX(true);
-  } else if (character.body.velocity.x > 0) {
-    character.setFlipX(false);
-  }
-
-  //   if (character.body.velocity.x !== 0 || character.body.velocity.y !== 0) {
-  //     character.anims.play("walk", true);
-  //   } else {
-  //     character.anims.play("idle", true);
-  //   }
-}
